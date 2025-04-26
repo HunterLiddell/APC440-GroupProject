a9906dd49d4d8b03c5f96839b7753120566d19ae
Liddellh <79621666+HunterLiddell@users.noreply.github.com>
2025-04-25 20:09:07 -0500

    Removed server files

--- apc440-group-project/src/routes/menu/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import { getFirestore } from "firebase-admin/firestore";
-import type { PageServerLoad } from "./$types";
-import { getMenuItemsAsync } from "$lib/services/db/menu-items";
-
-export const load: PageServerLoad = async ({ locals }) => {
-
-    
-}
