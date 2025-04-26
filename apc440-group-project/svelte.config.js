5c151a51a7a84a200c7de57bb27eb5173ef53d58
Liddellh <79621666+HunterLiddell@users.noreply.github.com>
2025-04-25 21:29:52 -0500

    Revert "Trying absolute paths"
    This reverts commit ffa834d4acc3b49e356d5a4b9dab3083b00f35a7.

--- apc440-group-project/svelte.config.js
+++ apc440-group-project/svelte.config.js
@@ -6,9 +6,6 @@ const config = {
 		outDir: 'dist'
 	},
 	kit: {
-		paths: {
-			relative: false,
-		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
