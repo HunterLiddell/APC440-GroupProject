2b93f30e1cad38530ddf57f80c7e6efedfe0c683
Liddellh <79621666+HunterLiddell@users.noreply.github.com>
2025-04-25 21:29:42 -0500

    Revert "Turning this into node project"
    This reverts commit 4f1a1d69a6bdf70fbebd1a69911ef852e8b63cd4.

--- apc440-group-project/svelte.config.js
+++ apc440-group-project/svelte.config.js
@@ -1,5 +1,4 @@
-import node from '@sveltejs/adapter-node';
-import {sveltePreprocess} from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,16 +7,19 @@ const config = {
 	},
 	kit: {
 		paths: {
-			relative: false
+			relative: false,
 		},
-
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: node({ out: 'out' }),
-
-    preprocess: [sveltePreprocess()]
-	},
+		adapter: adapter({
+			// The output directory (default is 'build')
+			pages: 'dist',
+			assets: 'dist',
+			fallback: 'index.html', // Enables single-page app routing
+			precompress: false
+		})
+	}
 };
 
 export default config;
