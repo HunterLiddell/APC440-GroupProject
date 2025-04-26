1788b2da0d11a4d30b0d9adffb2eb89f72b8e7d3
Liddellh <79621666+HunterLiddell@users.noreply.github.com>
2025-04-25 19:02:14 -0500

    Parsing json

--- apc440-group-project/src/lib/services/auth/firebase-admin.ts
+++ apc440-group-project/src/lib/services/auth/firebase-admin.ts
@@ -2,12 +2,15 @@ import { initializeApp, applicationDefault, cert, getApps } from "firebase-admin
 import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 
-const CREDS = process.env.SERVICE_ACCOUNT;
+const CREDS = process.env.SERVICE_ACCOUNT
+  ? JSON.parse(process.env.SERVICE_ACCOUNT)
+  : "/Users/hunterliddell/Desktop/APC440-GroupProject/apc440-group-project/src/lib/services/auth/paws-and-pastries-service-account.json";
+
 
 // Only initialize once (important for dev server hot reload)
 if (getApps().length === 0) {
   initializeApp({
-    credential: cert(CREDS ?? "/Users/hunterliddell/Desktop/APC440-GroupProject/apc440-group-project/src/lib/services/auth/paws-and-pastries-service-account.json")
+    credential: cert(CREDS)
     // credential: cert("/Users/hunterliddell/Desktop/APC440-GroupProject/apc440-group-project/src/lib/services/auth/paws-and-pastries-service-account.json"),
   });
 }
