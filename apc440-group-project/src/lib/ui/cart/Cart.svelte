3e954e6e5f36281b879eb98ec415a7e76bf74527
Liddellh <79621666+HunterLiddell@users.noreply.github.com>
2025-04-25 21:29:18 -0500

    Revert "Build again?"
    This reverts commit 0246d502de000e878511ef2c4ece279e501259f5.

--- apc440-group-project/src/lib/ui/cart/Cart.svelte
+++ apc440-group-project/src/lib/ui/cart/Cart.svelte
@@ -1,5 +1,4 @@
 <script module>
-	import { SvelteMap } from 'svelte/reactivity';
 	interface LineItem extends MenuItem {
 		quantity: number;
 	}
@@ -81,6 +80,7 @@
  Closing animation is the reverse of the opening. -->
 <script lang="ts">
 	import { X, ShoppingCart, Trash2 } from 'lucide-svelte';
+	import { SvelteMap } from 'svelte/reactivity';
 	import { on } from 'svelte/events';
 	import { onDestroy, onMount } from 'svelte';
 	import type { MenuItem } from '$lib/services/db/menu-items';
